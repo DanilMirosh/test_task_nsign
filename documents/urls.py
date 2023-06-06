1219c4e396687ecd2cc0dc14df856b30b797cc4c
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-06 23:47:41 +0500

    edit urls

---
+++ documents/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    CreateDocumentView,
+    EditDocumentView,
+    ViewDocumentView,
+    DeleteDocumentView,
+    CompareVersionsView,
+    DocumentListView,
+)
+
+urlpatterns = [
+    path('create/', CreateDocumentView.as_view(), name='create_document'),
+    path('edit/<int:document_id>/', EditDocumentView.as_view(), name='edit_document'),
+    path('view/<int:document_id>/', ViewDocumentView.as_view(), name='view_document'),
+    path('delete/<int:document_id>/', DeleteDocumentView.as_view(), name='delete_document'),
+    path('compare/<int:document_id>/', CompareVersionsView.as_view(), name='compare_versions'),
+    path('', DocumentListView.as_view(), name='document_list'),
+]
