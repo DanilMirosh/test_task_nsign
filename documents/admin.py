b835b6aca97914b85820b65f05432531d61e2bcd
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-06-07 22:08:10 +0500

    fix  admin

--- documents/admin.py
+++ documents/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Document, DocumentVersion
+from django.contrib.auth.models import Group
 
 admin.site.register(Document)
 admin.site.register(DocumentVersion)
+
+admin.site.unregister(Group)
